bd62ec7797da297d7eb8c24fdd6c128a62cadad9
253423-boop <253423@it2id.upchiapas.edu.mx>
2025-10-25 20:23:34 -0600

    primer commit

---
+++ ejercico 3.cpp
@@ -0,0 +1,17 @@
+// ejercico 3.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char equipoTipo[15];
+    char motivoBaja[60];
+    printf("Ingresa el tipo de equipo a dar de baja:\n");
+    fgets(equipoTipo);
+
+    
+}
+
